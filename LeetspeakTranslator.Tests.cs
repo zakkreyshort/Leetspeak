411bee30afb0786f4340c8b6a0a6bfc18f7749b7
Eric Settels, Michelle Morin and Zakkrey Short <eric@>
2020-03-02 09:51:29 -0800

    fail test for converting s to z

--- LeetspeakTranslator.Tests.cs
+++ LeetspeakTranslator.Tests.cs
@@ -7,40 +7,47 @@ namespace Leetspeak.Tests
   [TestClass]
   public class LeetspeakTranslatorTests
   {
-   [TestMethod]
-   public void Translate_SplitsStringIntoArray_Array()
-   {
-     char[] array = { 'h', 'l', 'l' };
-     LeetspeakTranslator translator = new LeetspeakTranslator();
-     CollectionAssert.AreEqual(array, translator.Translate("hll"));
-   }
-   [TestMethod]
-   public void Translate_SplitsStringIntoArray_ArrayChangeE()
-   {
-     char[] array = { 'h', '3', 'l', 'l' };
-     LeetspeakTranslator translator = new LeetspeakTranslator();
-     CollectionAssert.AreEqual(array, translator.Translate("hell"));
-   }
-   [TestMethod]
-   public void Translate_SplitsStringIntoArray_ArrayChangeO()
-   {
-     char[] array = { 'h', '3', 'l', 'l', '0' };
-     LeetspeakTranslator translator = new LeetspeakTranslator();
-     CollectionAssert.AreEqual(array, translator.Translate("hello"));
-   }
-   [TestMethod]
-   public void Translate_SplitsStringIntoArray_ArrayChangeI()
-   {
-     char[] array = { '1', ' ', 'd', '0' };
-     LeetspeakTranslator translator = new LeetspeakTranslator();
-     CollectionAssert.AreEqual(array, translator.Translate("I do"));
-   }
     [TestMethod]
-   public void Translate_SplitsStringIntoArray_ArrayChangeT()
-   {
-     char[] array = { 'L', '3', '7', 's', ' ', 'g', '0' };
-     LeetspeakTranslator translator = new LeetspeakTranslator();
-     CollectionAssert.AreEqual(array, translator.Translate("Lets go"));
-   }  
+    public void Translate_SplitsStringIntoArray_Array()
+    {
+      char[] array = { 'h', 'l', 'l' };
+      LeetspeakTranslator translator = new LeetspeakTranslator();
+      CollectionAssert.AreEqual(array, translator.Translate("hll"));
+    }
+    [TestMethod]
+    public void Translate_SplitsStringIntoArray_ArrayChangeE()
+    {
+      char[] array = { 'h', '3', 'l', 'l' };
+      LeetspeakTranslator translator = new LeetspeakTranslator();
+      CollectionAssert.AreEqual(array, translator.Translate("hell"));
+    }
+    [TestMethod]
+    public void Translate_SplitsStringIntoArray_ArrayChangeO()
+    {
+      char[] array = { 'h', '3', 'l', 'l', '0' };
+      LeetspeakTranslator translator = new LeetspeakTranslator();
+      CollectionAssert.AreEqual(array, translator.Translate("hello"));
+    }
+    [TestMethod]
+    public void Translate_SplitsStringIntoArray_ArrayChangeI()
+    {
+      char[] array = { '1', ' ', 'd', '0' };
+      LeetspeakTranslator translator = new LeetspeakTranslator();
+      CollectionAssert.AreEqual(array, translator.Translate("I do"));
+    }
+    [TestMethod]
+    public void Translate_SplitsStringIntoArray_ArrayChangeT()
+    {
+      char[] array = { 'L', '3', '7', 's', ' ', 'g', '0' };
+      LeetspeakTranslator translator = new LeetspeakTranslator();
+      CollectionAssert.AreEqual(array, translator.Translate("Lets go"));
+    }
+    [TestMethod]
+    public void Translate_SplitsStringIntoArray_ArrayChangeS()
+    {
+      char[] array = { 's', '0', ' ', 'l', '3', '7', 'z', ' ', 'g', '0' };
+      LeetspeakTranslator translator = new LeetspeakTranslator();
+      CollectionAssert.AreEqual(array, translator.Translate("so lets go"));
+    }  
   }
 }
