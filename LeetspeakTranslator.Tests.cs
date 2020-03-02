0bb44300b1223594f5096832f5ad3d3ac983bfd2
Eric Settels, Michelle Morin and Zakkrey Short <michelle@>
2020-03-02 09:17:05 -0800

    add specs, fail first test

---
+++ LeetspeakTranslator.Tests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetspeak;
+using Leetspeak.Models;
+
+namespace Leetspeak.Tests
+{
+  [TestClass]
+  public class LeetspeakTranslatorTests
+  {
+   [TestMethod]
+   public void Translate_SplitsStringIntoArray_Array()
+   {
+     char[] array = { 'h', 'e', 'l', 'l', 'o' };
+     LeetspeakTranslator translator = new LeetspeakTranslator();
+     Assert.AreEqual(array, translator.Translate("hello"));
+   }
+  }
+}
